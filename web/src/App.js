2e41ab56d328df1d9981235954df9ba41fc86533
Harry Waye <harry@arachnys.com>
2017-05-07 18:01:45 +0100

    Add simple RecipeSummary component
    Used function style rather than ES6 class style react component
    as seems a little simpler for now. Essentially going through
    the tutorial at https://facebook.github.io/react/docs/components-and-props.html

--- web/src/App.js
+++ web/src/App.js
@@ -1,18 +1,27 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+function RecipeSummary(props) {
+  const name = props.name;
+  return (
+    <div className="RecipeSummary">
+      <div className="RecipeSummaryName">
+        {name}
+      </div>
+    </div>
+  )
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h2>Welcome to Alcobot</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <RecipeSummary name="Whiskey Sour"/>
       </div>
     );
   }
